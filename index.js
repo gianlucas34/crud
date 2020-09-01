d3de21713112daf18a57af4509d68c1d9c88fc77
Gian Lucas <gianlucassmith34@gmail.com>
2020-09-01 14:37:40 -0700

    add mongo connection

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 mongoose.connect(
-    "mongodb+srv://deploy:gianlucas88@cluster0.qyhgi.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    process.env.MONGO_URL || 'mongodb://localhost:27017/crud',
     { useUnifiedTopology: true }
 );
 require('./src/models/Costumer')
